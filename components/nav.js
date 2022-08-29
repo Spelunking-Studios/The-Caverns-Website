10785810854847493a22c7e54dd4afd5878096d9
Deven McWhirk <93900347+DevBro123@users.noreply.github.com>
2022-08-29 16:50:59 +0000

    rvjegriuvgbe

--- components/nav.js
+++ components/nav.js
@@ -1,18 +1,24 @@
 import Link from "next/link";
 import ThemeToggle from "./theme-toggle.js";
+import dropdownStyles from "./dropdown.module.css";
+import { useState } from "react";
 
 export default function NavBar(props) {
+    const [dropdownActive, setDropdownActive] = useState(false);
+    const dropdownClick = () => {
+        setDropdownActive(!dropdownActive);
+    }
   return (
     <nav className="z-40 w-full h-14 dark:bg-zinc-800 bg-zinc-300 flex gap-6 fixed dark:text-neutral-200">
       <span></span>
-      <button id="dropdownMenu" data-dropdown-toggle="dropdown" type="button">
+      <button id="dropdownMenu" data-dropdown-toggle="dropdown" type="button" class={`${dropdownStyles.dropdownToggle}`} onClick={dropdownClick}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" className="transition ease-in-out w-8 hover:scale-125">
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
         </svg>
       </button>
 
-      <div id="dropdown">
-        <ul className="grid grid-col-1 py-1 text-sm bg-zinc-500 rounded-lg pr-5 pl-3 text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenu">
+      <div id="dropdown" class={`absolute top-14 ${dropdownStyles.dropdown}`} active={"" + dropdownActive}>
+        <ul className="grid grid-col-1 py-1 text-sm bg-zinc-600 rounded-b-lg pr-5 pl-3 text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenu">
         <Link href="/download">
           <a className="text-lg transition ease-in-out duration-100 p-1 hover:text-sky-500 dark:hover:text-sky-400 leading-loose">Download</a>
         </Link>
