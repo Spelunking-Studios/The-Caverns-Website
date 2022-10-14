e883a2416cdf3b69190225bf1a0988d70484f8c0
Ben Landon <81256789+https123456789@users.noreply.github.com>
2022-10-14 14:06:05 +0000

    index.js is now pretty

--- pages/index.js
+++ pages/index.js
@@ -20,11 +20,27 @@ export default function Home(props) {
 					<img className={styles["main-image"]} src="/images/CavernsLogo.webp"/>
 				</header>
 				<main className="flex-grow w-full mx-auto mb-0 dark:bg-zinc-700 dark:text-neutral-200">
-				<p className="text-center text-3xl pt-2 pb-2 mx-auto max-w-9/12">A top down indie action-adventure RPG.</p>
-                    <p className="text-center text-3xl pt-2 pb-2 mx-auto max-w-9/12">Venture through dark caverns, fighting many enemies along the way.</p>
+				<p style={{
+					textAlign: "center",
+					fontSize: "1.75rem",
+					paddingTop: "0.5rem",
+					paddingBottom: "0.5rem",
+					marginLeft: "auto",
+					marginRight: "auto",
+					maxWidth: "75%"
+				}}>A top down indie action-adventure RPG.</p>
+                    <p style={{
+					textAlign: "center",
+					fontSize: "1.75rem",
+					paddingTop: "0.5rem",
+					paddingBottom: "2.5rem",
+					marginLeft: "auto",
+					marginRight: "auto",
+					maxWidth: "75%",
+				}}>Venture through dark caverns, fighting many enemies along the way.</p>
 					<ImageCarousel images="/images/snap1.png;/images/snap2.png;/images/snap3.png" index={1}/>
 					<section className="mt-40 mx-auto rounded">
-						<p className="text-center text-4xl w-11/12 mb-20 mt-10 mx-auto pb-8 border-b-4 border-sky-500">Downloads</p>
+						<p className="page-section-title" style={{ marginTop: "7.5rem", marginBottom: "5rem", borderBottom: "0.25rem solid rgb(14 165 233)", width: "50%", marginLeft: "auto", marginRight: "auto" }}>Downloads</p>
 						<DownloadList/>
 					</section>
 				</main>
